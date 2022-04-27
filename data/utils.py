91e91028180aefa5b229987a9be4cc00d14c8c15
SGAKonata <yujiefan_jay@outlook.com>
2022-04-27 11:02:04 +0800

    fix pytorch barrier error when not using distribute mode
    try dist.barrier(), pass when error raised

--- data/utils.py
+++ data/utils.py
@@ -49,8 +49,10 @@ def save_result(result, result_dir, filename, remove_duplicate=''):
     final_result_file = os.path.join(result_dir, '%s.json'%filename)
     
     json.dump(result,open(result_file,'w'))
-
-    dist.barrier()
+    try:
+        dist.barrier()
+    except:
+        pass
 
     if utils.is_main_process():   
         # combine results from all processes
