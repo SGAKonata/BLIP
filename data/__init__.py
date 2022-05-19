a71fefdfc531fa5e5b274475e554c95193acf4d8
SGAKonata <yujiefan_jay@outlook.com>
2022-05-19 14:17:12 +0800

    fix: TypeError: '>' not supported between instances of 'int' and 'str'
    I sent wrong parameter to roco_train()

--- data/__init__.py
+++ data/__init__.py
@@ -41,7 +41,7 @@ def create_dataset(dataset, config, min_scale=0.5):
         return train_dataset, val_dataset, test_dataset
 
     elif dataset == 'caption_roco':
-        train_dataset = roco_train(transform_train, config['image_root'], 'train')
+        train_dataset = roco_train(transform_train, config['image_root'], prompt=config['prompt'])
         val_dataset = roco_eval(transform_test, config['image_root'], 'val')
         test_dataset = roco_eval(transform_test, config['image_root'], 'test')
         return train_dataset, val_dataset, test_dataset
